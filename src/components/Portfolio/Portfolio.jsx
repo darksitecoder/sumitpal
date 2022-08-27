ec76bf7d2af25cd3c467091041e03bb24ffd2568
Sumit Pal <50999497+darksitecoder@users.noreply.github.com>
2022-08-27 23:52:24 +0530

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/swiper.min.css';
 import Noot from "../../img/Noot.png";
 import BookMandir from "../../img/BookMandir.png";
-
+import Advisory from "../../img/advisory.png";
 import ApplianceRepairSite from "../../img/appliancerepairsite.png";
 import { themeContext } from "../../Context";
 
@@ -46,7 +46,11 @@ const Portfolio = () => {
               <SwiperSlide ><a href="https://bookmandir.vercel.app/" target="_blank"
             rel="noopener noreferrer"><img src={BookMandir} alt="" /></a></SwiperSlide>
               
-                <SwiperSlide style={{paddingRight:'200px'}}></SwiperSlide>            
+              <SwiperSlide ><a href="https://advices-app.netlify.app/" target="_blank"
+            rel="noopener noreferrer"><img src={Advisory} alt="" /></a></SwiperSlide>
+              
+              
+                <SwiperSlide style={{paddingRight:'20px'}}></SwiperSlide>            
         </Swiper>
         </div>
         </div>
