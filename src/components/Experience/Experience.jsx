9643c79489ae700569e9a94b4497d2d65e400ec0
Sumit Pal <50999497+darksitecoder@users.noreply.github.com>
2025-03-06 19:29:17 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,19 +8,19 @@ const Experience = () => {
   return (
     <div className="experience">
         <div className="achivement">
-            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>1+</div>
+            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>3+</div>
                 <span  style={{color: darkMode?'white':''}}>Years</span>
                 <span>Experience</span>
         </div>
 
         <div className="achivement">
-            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>20+</div>
+            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>50+</div>
                 <span  style={{color: darkMode?'white':''}}>Completed</span>
                 <span>Projects</span>
         </div>
 
         <div className="achivement">
-            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>1+</div>
+            <div className="circle" style={{color: darkMode?'var(--orange)':''}}>2+</div>
                 <span  style={{color: darkMode?'white':''}}>Companies</span>
                 <span>work</span>
         </div>
